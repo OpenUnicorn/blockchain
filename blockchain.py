9b2d2a76c4f14d6839a90890a8ada2b73170d66c
Vedant <vshetty.vs@gmail.com>
2018-02-04 17:05:44 +0530

    Completed new_transactions method

--- blockchain.py
+++ blockchain.py
@@ -2,13 +2,24 @@
 class Blockchain:
 	def __init__(self):
 		self.chain = [] #store the blockchain
-		self.transactions = [] # to store the transactons
+		self.transactions = [] # list of dictionary elements to store the transactons
 	def new_block(self):
 		#ToDO: Create a new block and add it to the chain
 		pass
 	def new_transaction(self):
-		#TODO: Add a new transaction to the list of transactions
-		pass
+		'''
+		:param sender: <string> Address of the sender
+		:param recipient: <string> Address of the Recipient
+		:param amount: <int> Amount
+		:return: <int>The index of the block that will hold this transaction
+		'''
+		self.transaction.append({
+		'sender': sender,
+		'recipient': recipient,
+		'amount': amount,
+		})
+		
+		return self.last_block['index']+1
 	
 	@staticmethod
 	def hash(block):
