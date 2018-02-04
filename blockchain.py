4dabdd8e253eabc5ef4e685c2f1fab8a4e486ff4
Vedant <vshetty.vs@gmail.com>
2018-02-04 15:04:15 +0530

    Created class structure of blockchain

--- blockchain.py
+++ blockchain.py
@@ -4,4 +4,18 @@ class Blockchain:
 		self.chain = [] #store the blockchain
 		self.transactions = [] # to store the transactons
 	def new_block(self):
+		#ToDO: Create a new block and add it to the chain
+		pass
+	def new_transaction(self):
+		#TODO: Add a new transaction to the list of transactions
+		pass
+	
+	@staticmethod
+	def hash(block):
+		#TODO: Hash the block
+		pass
+	
+	@property
+	def last_block(self):
+		#return the last block on the chain
 		pass
